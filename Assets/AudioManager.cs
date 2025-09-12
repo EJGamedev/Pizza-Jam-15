713055e34d96a274720a5e3ab9ae644f69a8a0a0
EJGamedev <ejgamedev06@gmail.com>
2025-09-12 17:43:36 -0500

    just testing commits. nothing works just yet, but art passes are possible for the phrases now

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip mainTrack;
+
+    //hi, wasn't expecting you here.
+    //these are private because ill have them automatically set themselves
+    private AudioSource mainSource;
+    private AudioSource phraseSource;
+    private AudioClip phraseClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
