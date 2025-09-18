cfc9e2159971dff63566e079c5b00775978693e6
unknown <herbiejames.dm@gmail.com>
2025-09-18 14:52:28 +0100

    import phrase audio

--- Assets/Scripts/Phrase.cs
+++ Assets/Scripts/Phrase.cs
@@ -1,7 +1,7 @@
-using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Phrase : MonoBehaviour
 {
@@ -21,6 +21,9 @@ public class Phrase : MonoBehaviour
     [Tooltip("Check true to have the gate check on that note")]
     public bool[] doCheck;
 
+    private List<int> noteByIndex = new List<int>();
+    private float metresPerBeat = 1.0f; // Only works for MPB = 1 :(
+
     [Header("Graphics Settings")]
     [Tooltip("Text displaying the cost")]
     public TextMeshPro displayText;
@@ -29,37 +32,41 @@ public class Phrase : MonoBehaviour
 
     private GameObject displayHold;
     private Vector3 holdScale, holdPosition;
-    private GameObject[] displayNotes;
+
+    // private GameObject[] displayNotes;
 
 
     private void Awake()
     {
-        List<int> noteByIndex = new List<int>();
-        float metresPerBeat = 1; // need a better solution to this (link to AM somehow?)
-        float holdLength = (doCheck.Length / beatDivisions) * metresPerBeat;
+        float holdLength = (doCheck.Length / beatDivisions) / metresPerBeat;
 
         displayText.text = cost.ToString();
 
-        if (holdLength < 1) { holdLength = 1; }
+        if (holdLength <= 1) { holdLength = 1; }
+        else { holdLength += 0.5f; }
         displayHold = GameObject.CreatePrimitive(PrimitiveType.Cube);
         displayHold.transform.parent = this.transform;
         displayHold.GetComponent<Renderer>().material = holdMaterial;
         Debug.Log(holdLength);
         displayHold.transform.localScale = new Vector3(0.75f, holdLength, 1.05f);
-        displayHold.transform.localPosition = new Vector3(0.0f, (holdLength/8.0f) - 0.125f, 0.0f);
+        displayHold.transform.position = this.transform.position;
+        displayHold.transform.position += new Vector3(0.0f, (holdLength/8.0f) - 0.125f, 0.0f);
+
+        /* THIS BREAKS THE GAME FOR SOME REASON :/
 
         for (int a = 0; a < doCheck.Length;) {
-            if (doCheck[a] == true) { noteByIndex.Add(a); }
+            if (doCheck[a] == true) {
+                noteByIndex.Add(a);
+                }
         }
-
-        for (int a = 1; a < noteByIndex.Count;) {
+        for (int a = 0; a < noteByIndex.Count;) {
+            if (a == 0) { continue; }
             float noteDistance = (noteByIndex[a] / beatDivisions) * metresPerBeat;
             displayNotes[a] = GameObject.CreatePrimitive(PrimitiveType.Cube);
             displayNotes[a].transform.parent = this.transform;
             displayNotes[a].GetComponent<Renderer>().material = noteMaterial;
             displayHold.transform.localPosition = new Vector3(0.0f, noteDistance, 0.0f);
         } 
+        */
     }
-
-
 }
